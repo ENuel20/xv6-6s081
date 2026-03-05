22f7fd7ae694da17e9a2b6423e68328c499d4f92
Onana <99745874+ENuel20@users.noreply.github.com>
2026-03-05 19:18:43 +0100

    Delete sleep/target directory

--- sleep/target/debug/deps/sleep-859306e72f0328e4.d
+++
@@ -1,5 +0,0 @@
-/home/user_nuel21/xv6-6s081/sleep/target/debug/deps/sleep-859306e72f0328e4.d: src/main.rs
-
-/home/user_nuel21/xv6-6s081/sleep/target/debug/deps/sleep-859306e72f0328e4: src/main.rs
-
-src/main.rs:
--- sleep/target/debug/sleep.d
+++
@@ -1,1 +0,0 @@
-/home/user_nuel21/xv6-6s081/sleep/target/debug/sleep: /home/user_nuel21/xv6-6s081/sleep/src/main.rs
